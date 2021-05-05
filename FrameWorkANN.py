f8721768288ce22853a62dbef143090f631de159
noam <kotlar@post.bgu.ac.il>
2021-05-05 20:12:47 +0300

    f1, presc, rec

--- FrameWorkANN.py
+++ FrameWorkANN.py
@@ -1,4 +1,5 @@
 from sklearn.feature_selection import SelectKBest, chi2
+from sklearn.metrics import confusion_matrix, classification_report, precision_recall_fscore_support
 from sklearn.neural_network import MLPClassifier
 from sklearn.svm import SVC
 import pandas as pd
@@ -10,26 +11,35 @@ class FrameWorkANN(Strategy):
     def __init__(self, model,TrainPath,TestPath,param=None):
         super().__init__(model,"ANN",TrainPath,TestPath, param)
         self.coef_ = None
+        self.recall = None
+        self.prec = None
+        self.f1 = None
 
     def train(self):
         super().train()
         return self.accur
 
-    def getFeatureImportance(self):
-        '''
-        optional: Feature inmportance
-        :return:
-        '''
-        pass
+    def get_feature_importance(self):
+        mesures= precision_recall_fscore_support(self.y_test, self.prediction, average='weighted')
+        self.recall =mesures[0]
+        self.prec = mesures[1]
+        self.f1 = mesures[2]
 
     def getCsvData(self):
 
         train = self.x_test.columns.values
         columnAsList = list(train)
 
+        #predictions measures
         parameters = self.param
-        parameters['accurancy'] = self.accur
         parameters['numOfF'] = len(columnAsList)
+        self.get_feature_importance()
+        parameters['recall'] = self.recall
+        parameters['precision'] = self.prec
+        parameters['f1'] = self.f1
+        parameters['accuracy'] = self.accur
+
+        #columns names
         colums = list(parameters.keys())
         df = pd.DataFrame(parameters.items()).T
         df.columns =colums
@@ -43,12 +53,12 @@ def checkANN():
 
     clf = MLPClassifier()
     parameter_space = {
-        'hidden_layer_sizes': [(5,10,5),(15,),(5,5,5),(8,8),(100,5,100)],
+        # 'hidden_layer_sizes': [(5,10,5),(15,),(5,5,5),(8,8),(100,5,100)],
         'activation': ['tanh', 'relu'],
         # 'solver': ['sgd', 'adam'],
         'alpha': [0.0001, 0.05],
         # 'learning_rate': ['constant', 'invscaling', 'adaptive'],
-        'max_iter':[1,200]
+        'max_iter':[200,500]
     }
     model = Context(FrameWorkANN(clf,'trainData26F.csv','TestData26F.csv',param=parameter_space,))
     model.strategy.x_train = SelectKBest(chi2, k=8).fit_transform(model.strategy.x_train, model.strategy.y_train)
@@ -58,7 +68,7 @@ def checkANN():
     model.strategy.grid_search()
     model.run_model()
     data = model.strategy.getCsvData()
-    model.strategy.insertDataToCSV(data, "2")
+    model.strategy.insertDataToCSV(data, "3")
 
 
 checkANN()
