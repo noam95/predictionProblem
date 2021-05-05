bf9ba358b63d1ea44f137e52d4c4cdc0986ddef6
hallelhel <hallelh@post.bgu.ac.il>
2021-05-05 14:53:07 +0300

    checkRandom - updates for plots to csv

--- FrameWorkRandomForest.py
+++ FrameWorkRandomForest.py
@@ -1,5 +1,5 @@
 from sklearn.ensemble import RandomForestClassifier
-
+import pandas as pd
 from Context import Strategy, Context
 
 
@@ -18,13 +18,29 @@ class FrameWorkRandomForest(Strategy):
         :return:
         '''
         pass
-
-
+    def get_feature_importance(self, model):
+        return self.model.feature_importances_
 
 
 def checkRandomForest():
-    RandomForest  = RandomForestClassifier(random_state=0)
+    RandomForest = RandomForestClassifier(random_state=0)
     parameter_space = []
-    model = Context(FrameWorkRandomForest(RandomForest, param=parameter_space))
-    model.strategy.grid_search()
+    model = Context(FrameWorkRandomForest(RandomForest, "TestData26F.csv", "trainData26F.csv"))
     accurancy_model = model.run_model()
+    importance = RandomForest.feature_importances_
+    train = model.strategy.x_train
+    columns = train.columns.values
+    data_df = {'Model name': ["Random Forest"],
+                'Number of features': [len(columns)],
+                'Accurancy': [model.strategy.accur]
+                }
+    i = 0
+    for col in columns[0:len(train.columns) - 1]:
+        data_df[col] = importance[i]
+        i += 1
+
+    df = pd.DataFrame(data_df)
+    model.strategy.insertDataToCSV(data_df)
+
+
+checkRandomForest()
